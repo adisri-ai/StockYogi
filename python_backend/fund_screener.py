2359a800729b4c4407a0b3e9134b21129d6deaed
Aditya Shrivastava <srivastavaaditya3@gmail.com>
2026-01-04 11:23:24 +0530

    Create fund_screener.py

---
+++ python_backend/fund_screener.py
@@ -0,0 +1,6 @@
+"""
+This file contains the main core logic for fund allocation algorithm that
+optimises the user mututal fund portfolio based on historical data and the 
+user's risk-taking-capability.
+It takes as input the worst possible annual return a user can bear in a bad market year
+"""
