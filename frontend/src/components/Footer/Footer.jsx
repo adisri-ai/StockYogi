4924c81860f37d98248feba0ad4873a0699e438e
Aditya Shrivastava <srivastavaaditya3@gmail.com>
2026-01-16 21:38:03 +0530

    Footer

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>© {new Date().getFullYear()} StockVision — All Rights Reserved</p>
+    </footer>
+  );
+}
